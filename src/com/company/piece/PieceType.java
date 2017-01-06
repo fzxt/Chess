e8cad59e25cd593ac9ec8f2e697298e198c9c415
ilaskaev <i.laskaev@gmail.com>
2017-01-05 19:03:58 -0500

    Declared piece types

---
+++ src/com/company/piece/PieceType.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.piece;
+
+/**
+ *
+ * @author il13eo
+ */
+public enum PieceType {
+    KING,
+    QUEEN,
+    ROOK,
+    KNIGHT,
+    BISHOP,
+    PAWN
+}
