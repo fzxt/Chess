f5d3ecfaf0bca90d5ff47f7dbacdf99e1e6318a9
ilaskaev <i.laskaev@gmail.com>
2017-01-05 19:34:24 -0500

    Updated pieces and move class

--- src/com/company/piece/Pawn.java
+++ src/com/company/piece/Pawn.java
@@ -1,6 +1,7 @@
 package com.company.piece;
 
 import com.company.Team;
+import com.company.board.Board;
 import com.company.board.Move;
 import java.util.ArrayList;
 
@@ -9,9 +10,9 @@ public class Pawn extends Piece {
     public Pawn(Team team, int value, PieceType type){
         super(team, value, type);
     }
-    
+
     @Override
-    public ArrayList<Move> getAvailableMoves() {
+    public ArrayList<Move> getAvailableMoves(Board board) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
