a13ff02f6008661361304e7a7d814771332bc30c
ilaskaev <i.laskaev@gmail.com>
2017-01-05 19:11:06 -0500

    Created dummy pieces

--- src/com/company/piece/Piece.java
+++ src/com/company/piece/Piece.java
@@ -6,7 +6,6 @@ import com.company.board.Move;
 import java.util.ArrayList;
 
 public abstract class Piece {
-
     private Team team;
     private PieceType type;
     private int value;
---
+++ src/com/company/piece/Queen.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.piece;
+
+import com.company.Team;
+import com.company.board.Move;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author il13eo
+ */
+public class Queen extends Piece {
+
+    public Queen(Team team, int value, PieceType type){
+        super(team, value, type);
+    }
+    
+    @Override
+    public ArrayList<Move> getAvailableMoves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
