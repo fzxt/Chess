f064f72cbcc76b38738442a0f0c3808653866af6
fzxt <fahadahmad41@hotmail.com>
2017-01-05 20:47:57 -0500

    Added moves to pawn

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,9 +1,17 @@
 package com.company;
 
+import com.company.board.Board;
+import com.company.piece.Pawn;
+import com.company.piece.Piece;
+import com.company.piece.PieceType;
+import java.awt.Point;
+
 public class Game {
 
     public Game() {
         // Initialization logic here
+        Piece p = new Pawn(Team.BLACK, 0, PieceType.PAWN, new Point(1, 1));
+        p.getAvailableMoves(new Board());
     }
 
     public static void main(String[] args) {
